bd9bfa8ec240eaecf615a959f4b505d881d0406f
Jorge Nava de la Peña <jorge.nava@Jorges-MacBook-Pro.local>
2025-05-02 16:10:33 -0600

    Update generateUploadUrl lambda to generate also S3Key

--- lambdas/generateUploadUrl/index.js
+++ lambdas/generateUploadUrl/index.js
@@ -1,20 +1,24 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
-const { v4: uuidv4 } = require('uuid');
+const { getSignedUrl }              = require('@aws-sdk/s3-request-presigner');
+const { v4: uuidv4 }                = require('uuid');
 
-const client = new S3Client();
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 exports.handler = async () => {
   const key = `${uuidv4()}.mp3`;
-  const command = new PutObjectCommand({
-    Bucket: process.env.AUDIO_BUCKET,
-    Key: key
+  const cmd = new PutObjectCommand({
+    Bucket:      process.env.AUDIO_BUCKET,
+    Key:         key,
+    ContentType: 'audio/mpeg'
   });
 
-  const url = await getSignedUrl(client, command, { expiresIn: 300 });
+  const uploadUrl = await getSignedUrl(s3, cmd, { expiresIn: 300 });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ uploadUrl: url, s3Key: key })
+    body: JSON.stringify({
+      uploadUrl,
+      s3Key: key
+    })
   };
 };
