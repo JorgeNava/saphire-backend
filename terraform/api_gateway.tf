07d76b2801d2ed22c4d780280c26a17325edf6d1
Jorge Nava de la Peña <jorge.nava@Jorges-MacBook-Pro.local>
2025-05-27 21:07:24 -0600

    Update readme

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,5 +1,18 @@
 # terraform/api_gateway.tf
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 #################################################################
 # 1. HTTP API
 #################################################################
