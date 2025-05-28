e35ac73780c0481763d001d0b38bcd994812377e
Jorge Nava de la Peña <jorge.nava@Jorges-MacBook-Pro.local>
2025-05-27 18:28:36 -0600

    Update deploy.yml

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "zafira-terraform-state"     # nombre de tu bucket S3
+    bucket         = "zafira-terraform-states"     # nombre de tu bucket S3
     key            = "env/Zafira/terraform.tfstate" # prefijo fijo y ruta
     region         = "us-east-1"                  # región del bucket y locks
     dynamodb_table = "zafira-terraform-locks"     # tabla para locking
