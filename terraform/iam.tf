2fc44dbc28c853c32fd9a824da9a3590420962f7
Jorge Nava de la Peña <jorge.nava@Jorges-MacBook-Pro.local>
2025-05-29 18:28:00 -0600

    Add more policies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,66 +1,28 @@
-// -----------------------------------------------------------------------------
-// iam.tf
-// Define el rol de ejecución para todas las Lambdas y permisos DDB con acceso a GSIs
-// -----------------------------------------------------------------------------
+###############################################################################
+# iam.tf
+# Define el rol que usan las Lambdas y sus políticas de acceso a DynamoDB & CloudWatch
+###############################################################################
 
-# 1) Creamos un local para el nombre del rol a partir de table_prefix
-locals {
-  lambda_execution_role_name = "${var.table_prefix}-lambda-exec-role"
-}
+# Obtiene la cuenta actual para interpolar en los ARNs
+data "aws_caller_identity" "current" {}
 
+# 1) Rol de ejecución para Lambda
 resource "aws_iam_role" "lambda_exec" {
-  name = local.lambda_execution_role_name
+  name = "${var.table_prefix}-lambda-exec-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Action    = "sts:AssumeRole"
-      Principal = { Service = "lambda.amazonaws.com" }
       Effect    = "Allow"
-      Sid       = ""
+      Principal = { Service = "lambda.amazonaws.com" }
     }]
   })
 }
 
-# Permisos básicos de CloudWatch Logs
-resource "aws_iam_role_policy_attachment" "lambda_logging" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Acceso a DynamoDB
-resource "aws_iam_role_policy" "lambda_ddb_access" {
-  name = "${var.table_prefix}-lambda-ddb-access"
-  role = aws_iam_role.lambda_exec.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = [
-        "dynamodb:PutItem",
-        "dynamodb:GetItem",
-        "dynamodb:UpdateItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:Scan",
-        "dynamodb:Query"
-      ]
-      Effect   = "Allow"
-      Resource = [
-        aws_dynamodb_table.users.arn,
-        aws_dynamodb_table.messages.arn,
-        aws_dynamodb_table.thoughts.arn,
-        aws_dynamodb_table.lists.arn,
-        aws_dynamodb_table.notes.arn,
-        aws_dynamodb_table.tags.arn,
-        aws_dynamodb_table.actions_log.arn
-      ]
-    }]
-  })
-}
-
-# Permiso para invocar otras Lambdas (ej. tu intent-identifier y las 3 dispatch)
-resource "aws_iam_role_policy" "lambda_invoke" {
-  name = "${var.table_prefix}-lambda-invoke-others"
+# 2) Permisos para escribir logs en CloudWatch
+resource "aws_iam_role_policy" "lambda_logs" {
+  name = "${var.table_prefix}-lambda-logs"
   role = aws_iam_role.lambda_exec.id
 
   policy = jsonencode({
@@ -68,13 +30,40 @@ resource "aws_iam_role_policy" "lambda_invoke" {
     Statement = [
       {
         Effect = "Allow"
-        Action = "lambda:InvokeFunction"
-        # Puedes usar ARN exacto o wildcard para todas tus funciones:
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# 3) Permisos completos de lectura/escritura/consulta en DynamoDB
+resource "aws_iam_role_policy" "lambda_ddb_access" {
+  name = "${var.table_prefix}-lambda-ddb-access"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ]
         Resource = [
-          aws_lambda_function.all["messageIntentIdentification"].arn,
-          aws_lambda_function.all["createThought"].arn,
-          aws_lambda_function.all["createListThroughAI"].arn,
-          aws_lambda_function.all["performResearch"].arn
+          # Todas las tablas cuyo nombre empiece con el prefijo configurado
+          "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.table_prefix}-*",
+          # Todos los índices (GSI) de esas tablas
+          "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.table_prefix}-*/index/*"
         ]
       }
     ]
